5de388d9e0ef5d83a657d52f6dfcb62fd19ed52c
rizavelioglu <rzavelioglu@gmail.com>
2020-11-28 23:03:15 +0100

    Create concat_memotion&hm.py

---
+++ utils/concat_memotion&hm.py
@@ -0,0 +1,77 @@
+"""
+train_v10.jsonl includes:
+    - train.jsonl (8500)
+    - 100 images from 'dev_seen.jsonl' that are not in 'dev_unseen.jsonl' (100)
+    - images from memotion dataset (243)
+    so in total of 8500+100+243 = 9743 images
+"""
+import pandas as pd
+
+
+def get_img_list_from_dev(path_to_annotations):
+    """
+    'dev_seen.jsonl'(0.5k) & 'dev_unseen.jsonl'(0.54k) have 400 identical images.
+    This function extracts those 100 images from 'dev_seen.jsonl' that are not in
+    'dev_unseen.jsonl' in order to add them into 'train.jsonl' for increasing
+    dataset size.
+
+    :param path_to_annotations: Path to annotation .json files
+    :return: List of image paths in 'dev_seen.jsonl'
+    """
+    val_seen = pd.read_json(f"{path_to_annotations}/dev_seen.jsonl", lines=True)["img"]
+    val_unseen = pd.read_json(f"{path_to_annotations}/dev_unseen.jsonl", lines=True)["img"]
+
+    diff_ids = list(set(val_seen).symmetric_difference(val_unseen))
+
+    val_seen_imgs = []
+    val_unseen_imgs = []
+    for img in diff_ids:
+        if img in list(val_seen):
+            val_seen_imgs.append(img)
+        elif img in list(val_unseen):
+            val_unseen_imgs.append(img)
+        else:
+            print("Error occured at img: ", img)
+
+    assert len(val_seen_imgs)==100 , "Error in counting 'dev_seen'"
+    assert len(val_unseen_imgs)==140, "Error in counting 'dev_unseen'"
+    assert sum([i in list(val_unseen) for i in val_seen_imgs])==0, "Error: some images are in both dev files!"
+
+    return val_seen_imgs
+
+
+# Get annotations
+dev_seen = pd.read_json("/root/.cache/torch/mmf/data/datasets/hateful_memes/defaults/annotations/dev_seen.jsonl", lines=True)
+dev_unseen = pd.read_json("/root/.cache/torch/mmf/data/datasets/hateful_memes/defaults/annotations/dev_unseen.jsonl", lines=True)
+train = pd.read_json("/root/.cache/torch/mmf/data/datasets/hateful_memes/defaults/annotations/train.jsonl", lines=True)
+
+# Get 100 image id's: {'dev_seen' \ 'dev_unseen'}
+seen_imgs = get_img_list_from_dev("/root/.cache/torch/mmf/data/datasets/hateful_memes/defaults/annotations/")
+
+# Add 100 images to 'train.jsonl'
+for i in seen_imgs:
+    train = pd.concat([train, dev_seen[dev_seen["img"]==i]], axis=0)
+
+# Add 'dev_unseen.jsonl' to 'train.jsonl'
+final_data = pd.concat([train, dev_unseen], axis=0)
+
+##############################################################################
+# Check if feature is extracted for an image from memotion
+memotion = pd.read_json("./label_memotion.jsonl", lines=True)
+tmp =list( pd.read_csv("./extracted_feats_memotion.csv")["img"])
+
+new_memo = pd.DataFrame()
+
+for i in memotion["img"]:
+    if i.split("/")[1].split(".")[0]+".npy" in tmp:
+        new_memo = pd.concat([new_memo, memotion[memotion["img"]==i]])
+##############################################################################
+# Add memotion data to 'train.jsonl'
+train = pd.concat([final_data, new_memo])
+# Shuffle data
+train = train.sample(frac=1).reset_index(drop=True)
+# Write new jsonl file
+train_json = train.to_json(orient='records', lines=True)
+
+with open("train_v10.jsonl", "w", encoding='utf-8') as f:
+    f.write(train_json)
