1b24df494a9856fddbbb6551d98c892f85789640
rizavelioglu <rzavelioglu@gmail.com>
2020-12-01 04:14:55 +0100

    Create generate_submission.sh

---
+++ utils/generate_submission.sh
@@ -0,0 +1,12 @@
+mmf_predict config="projects/visual_bert/configs/hateful_memes/defaults.yaml" \
+    model="visual_bert" \
+    dataset=hateful_memes \
+    run_type=test \
+    checkpoint.resume_file=$1 \
+    checkpoint.reset.optimizer=True \
+    dataset_config.hateful_memes.annotations.val[0]=hateful_memes/defaults/annotations/dev_unseen.jsonl \
+    dataset_config.hateful_memes.annotations.test[0]=hateful_memes/defaults/annotations/test_unseen.jsonl \
+    dataset_config.hateful_memes.features.train[0]=$2 \
+    dataset_config.hateful_memes.features.val[0]=$2 \
+    dataset_config.hateful_memes.features.test[0]=$2 \
+    env.save_dir="./save/preds"
